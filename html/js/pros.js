2cba50d102789fa8ac6e36e0788bfaebd8cf5283
paul.vudmaska <paul@admins-MacBook-Pro-2.local>
2010-04-15 09:39:10 -0500

    add proto

--- html/js/pros.js
+++ html/js/pros.js
@@ -1,3 +1,11 @@
 String.prototype.xpnd = function(keys) {
   return this.replace(/\#\{(.*?)\}/g, function(x,y) { return keys[y]});
 }
+
+String.prototype.trim = function(){
+  return this.replace(/^\s+/,'').replace(/\s+$/,'')
+}    
+
+String.prototype.empty = function(){
+  return (this.trim() == '')
+}
