fae2f5fd550a9b77789e10ee42144bdcd3ecc02f
Paul Vudmaska <paul@vudmaska.com>
2011-05-27 11:37:55 -0500

    added files

---
+++ marklets/cam/boot.js
@@ -0,0 +1,20 @@
+var URL = 'https://github.com/voodoo/scraps/raw/master/marklets/';
+
+function css(href){
+	var link = $("<link>");
+	link.attr({
+	        type: 'text/css',
+	        rel: 'stylesheet',
+	        href: URL + href
+	});
+	link.onload(function(){
+		console.log('css loaded ' + href); 
+	});
+	$("head").append( link ); 
+	
+}
+css('app.css');
+
+$.getScript(URL + 'app.js', function(){
+	console.log(' app is ready');
+});
