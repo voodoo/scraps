4fc163f0336c5123b40a26db8d396223c405275f
Paul Vudmaska <paul@vudmaska.com>
2011-05-27 11:52:34 -0500

    :

--- marklets/cam/boot.js
+++ marklets/cam/boot.js
@@ -1,20 +1,21 @@
-var URL = 'https://github.com/voodoo/scraps/raw/master/marklets/cam/';
-
-function css(href){
-	var link = $("<link>");
-	link.attr({
-	        type: 'text/css',
-	        rel: 'stylesheet',
-	        href: URL + href
-	});
-	link.onload(function(){
-		console.log('css loaded ' + href); 
-	});
-	$("head").append( link ); 
-	
-}
-css('app.css');
-
-$.getScript(URL + 'app.js', function(){
-	console.log(' app is ready');
-});
+console.log(typeof $);
+// var URL = 'https://github.com/voodoo/scraps/raw/master/marklets/cam/';
+// 
+// function css(href){
+// 	var link = $("<link>");
+// 	link.attr({
+// 	        type: 'text/css',
+// 	        rel: 'stylesheet',
+// 	        href: URL + href
+// 	});
+// 	link.onload(function(){
+// 		console.log('css loaded ' + href); 
+// 	});
+// 	$("head").append( link ); 
+// 	
+// }
+// css('app.css');
+// 
+// $.getScript(URL + 'app.js', function(){
+// 	console.log(' app is ready');
+// });
